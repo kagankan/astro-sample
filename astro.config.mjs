2b09012448110d5e7eb47b0fd2ab61f11f69dfc3
kagan <intend.on.games+git@gmail.com>
2022-12-29 00:36:59 +0900

    Create image integration sample

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,7 @@
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import image from '@astrojs/image';
 import { defineConfig } from 'astro/config';
-import autoprefixer from 'autoprefixer';
-import tailwind from 'tailwindcss';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  base: '/astro-sample',
-  vite: {
-    build: {
-      assetsInlineLimit: 0,
-    },
-    plugins: [viteCommonjs()],
-    css: {
-      postcss: {
-        plugins: [tailwind, autoprefixer],
-      },
-    },
-  },
+  integrations: [image()],
 });
